4a6ed359237d93db12dc0f2727cef8f33f9170f2
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-02-15 21:00:08 +0100

    fail on phase creation

---
+++ src/lib/phase.mli
@@ -0,0 +1,58 @@
+
+(* This file is free software, part of FTW. See file "LICENSE" for more information *)
+
+(* Type definitions *)
+(* ************************************************************************* *)
+
+type id = Id.t [@@deriving yojson]
+(** Ids for phases *)
+
+type t
+(** Phases *)
+
+
+(* Common functions *)
+(* ************************************************************************* *)
+
+val id : t -> id
+(** Unique id for the phase. *)
+
+val name : t -> string
+(** Name of the phase *)
+
+val competition : t -> Competition.id
+(** Parent competition for the phase. *)
+
+val order : t -> string
+(** Order (prelim/semi/final) of the phase *)
+
+val judge_artefact : t -> string
+(** Type of artefact for judges of the phase *)
+
+val head_judge_artefact : t -> string
+(** Type of artefact for head judge of the phase *)
+
+val ranking_algorithm : t -> string
+(** Ranking algorithm of the phase *)
+
+
+
+(* DB interaction *)
+(* ************************************************************************* *)
+
+val get : State.t -> id -> t
+(** Get an event from its id.
+    @raise Not_found if the phase is not found. *)
+
+val list : State.t -> t list
+(** Get the list of all phases. *)
+
+val from_competition : State.t -> Competition.id -> t list
+(** Get the list of all phases that belong to a given competition. *)
+
+val ids_from_competition : State.t -> Competition.id -> id list
+(** Get the list of all phases id that belong to a given competition. *)
+
+val create : State.t -> string -> Competition.id -> string -> string -> string -> string -> id
+(** Create a new phase *)
+
