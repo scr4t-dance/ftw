3fa2437fd325d46055fbebeba5d2f4657134bea7
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-03-09 22:29:53 +0100

    allow react dev

--- src/backend/main.ml
+++ src/backend/main.ml
@@ -44,11 +44,28 @@ let () =
     |> Event.routes
     |> Competition.routes
   in
+  (* Define CORS middleware manually *)
+  let cors_middleware handler request =
+    match Dream.method_ request with
+    | `OPTIONS ->
+      Dream.respond ~headers:[
+        ("Access-Control-Allow-Origin", "*");
+        ("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        ("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      ] ~status:`No_Content ""
+      | _ ->
+        let%lwt response = handler request in
+        Dream.add_header response "Access-Control-Allow-Origin" "*";
+        Dream.add_header response "Access-Control-Allow-Headers" "Content-Type, Authorization";
+        Lwt.return response
+  in
   (* Setup the dream server and run it *)
   Dream.run
     ~interface:"0.0.0.0"
     ~port:options.server_port
-  @@ Dream.logger
+    ~tls:true
+  @@ Dream.logger  
+  @@ cors_middleware
   @@ Dream.memory_sessions
   @@ State.init ~path:options.db_path
   @@ Router.build ~default_routes router
