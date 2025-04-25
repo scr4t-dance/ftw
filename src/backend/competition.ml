1044b4e99645adf484b5c39d298e97cfbcc43291
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-04-25 19:08:17 +0200

    temporary commit to add react-hook-form

--- src/backend/competition.ml
+++ src/backend/competition.ml
@@ -75,8 +75,8 @@ let rec routes router =
         ~description:"Invalid input";
     ]
   (* Event comps query *)
-  |> Router.get "/api/comp/:id/dancers" list_dancers
-    ~tags:["competition"; "dancer"]
+  |> Router.get "/api/event/:id/dancers" list_dancers
+    ~tags:["dancer"; "competition"]
     ~summary:"Get the list of dancers of a Competition"
     ~parameters:[
       Types.obj @@ Spec.make_parameter_object ()
@@ -98,7 +98,7 @@ let rec routes router =
           ~description:"Competition not found";
     ]
   |> Router.put "/api/comp/:id/bib" add_dancer
-    ~tags:["competition"; "dancer"]
+    ~tags:[ "dancer"; "competition"]
     ~summary:"Add dancer to competition"
     ~request_body:(
       Types.obj @@ Spec.make_request_body_object ()
@@ -195,6 +195,42 @@ and list_dancers =
        let bib_list_result = Result.map aux bib_map_result in
        let bib_list_with_error = Result.map_error
            Error.generic bib_list_result
+       in Result.map (fun bibs : Types.BibList.t -> { bibs; }) bib_list_with_error
+    )
+
+and add_dancer =
+  Api.put
+    ~of_yojson:Types.Bib.of_yojson
+    ~to_yojson:Types.DancerIdList.to_yojson
+    (
+      fun req st (bib : Types.Bib.t) ->
+        let+ id = Utils.int_param req "id" in
+        match bib.competition with
+        | comp_id when comp_id = id ->
+          let target = Types.Target.to_ftw bib.target in
+          Ftw.Bib.set ~st ~competition:id ~target ~bib:bib.bib;
+          let dancer_list : Types.DancerIdList.t = {dancers=Types.Target.dancers bib.target} in
+          Ok dancer_list
+        | _ -> Error (Error.generic "Competition id do not match payload")
+    )
+
+and list_dancers =
+  Api.get
+    ~to_yojson:Types.BibList.to_yojson
+    (fun req st ->
+       let+ id = Utils.int_param req "id" in
+       let bib_map_result = Ftw.Bib.list_from_comp ~st ~competition:id in
+       let bib_converter (bib, t) : Types.Bib.t =
+         {competition=id; bib=bib;target= Types.Target.of_ftw t}
+       in
+       let aux bib_map : Types.Bib.t list =
+         Ftw.Id.Map.to_seq bib_map
+         |> List.of_seq
+         |> List.map bib_converter
+       in
+       let bib_list_result = Result.map aux bib_map_result in
+       let bib_list_with_error = Result.map_error
+        Error.generic bib_list_result
        in Result.map (fun bibs : Types.BibList.t -> { bibs; }) bib_list_with_error
     )
 
