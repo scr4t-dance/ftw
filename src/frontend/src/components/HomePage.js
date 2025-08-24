945ef45f2c80f1a388d72935e71f773ea91ba950
dowymn <down@Dowymn.localdomain>
2025-08-24 14:08:06 +0200

    home page updates

--- src/frontend/src/components/HomePage.js
+++ src/frontend/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import "../styles/ContentStyle.css"
+import logo from "../assets/logo.png";
 
 import PageTitle from "./PageTitle";
 import Header from "./Header"
@@ -10,27 +11,31 @@ function HomePage() {
             <PageTitle title="SCR4T" />
             <Header />
             <div className="content-container">
-                <h1>SCR4T - Système Compétitif de Rock 4 Temps</h1>
+                <div>
+                       <img src={logo} id="homepage-logo" alt="Logo" />
+                       
+                        <h1>SCR4T - Système Compétitif de Rock 4 Temps</h1>
 
-                <h2>Présentation</h2>
-                <p>
-                    Le SCR4T, ou “Scrat” pour les amis et les fans de “l'Âge de Glace”, est le “Système Compétitif de Rock 4 Temps”. 
-                    C'est un système d'organisation de compétition pensé pour, et amené à évoluer avec la communauté de Rock 4 Temps 
-                    (ou toute autre appellation de danses de couple se considérant comme voisine de celle-ci) d'Île de France.
-                </p>
-                <p>Le SCR4T propose une architecture de compétition en 3 sous-systèmes complémentaires :</p>
-                <ul>
-                    <li>une organisation en divisions pour les danseur-euse-s, stable dans le temps et partagée par plusieurs compétitions ;</li>
-                    <li>une architecture de compétition prélims/demi-finales/finales, dont l'objectif est de rendre comparables les différentes compétitions entre elles ;</li>
-                    <li>un système de points, régulant le passage d'une division à une autre.</li>
-                </ul>
+                        <h2>Présentation</h2>
+                        <p>
+                            Le SCR4T, ou "Scrat" pour les amis et les fans de "l'Âge de Glace", est le "Système Compétitif de Rock 4 Temps". 
+                            C'est un système d'organisation de compétition pensé pour, et amené à évoluer avec la communauté de Rock 4 Temps 
+                            (ou toute autre appellation de danses de couple se considérant comme voisine de celle-ci) d'Île de France.
+                        </p>
+                        <p>Le SCR4T propose une architecture de compétition en 3 sous-systèmes complémentaires :</p>
+                        <ul>
+                            <li>Une organisation en divisions pour les danseur•euse•s, stable dans le temps et partagée par plusieurs compétitions ;</li>
+                            <li>Une architecture de compétition prélims/demi-finales/finales, dont l'objectif est de rendre comparables les différentes compétitions entre elles ;</li>
+                            <li>Un système de points, régulant le passage d'une division à une autre.</li>
+                        </ul>
+                </div>
 
                 <h2>Pourquoi un système de divisions à points ?</h2>
-                <p>Avec les années, la communauté de Rock 4 Temps grandit, et nous en sommes très heureux. Avec cette croissance, les écarts de niveau entre les débutant·e·s et les danseur-euse-s les plus aguerri·e·s croissent également.</p>
+                <p>Avec les années, la communauté de Rock 4 Temps grandit, et nous en sommes très heureux. Avec cette croissance, les écarts de niveau entre les débutant•e•s et les danseur•euse•s les plus aguerri•e•s croissent également.</p>
                 <p>Nous proposons un système de divisions à points pour faire de la compétition un parcours, qui puisse être motivant et épanouissant pour tout le monde, pour accompagner l'évolution d'un danseur-euse de Rock 4 Temps sur plusieurs paliers.</p>
                 <p>
-                    Chaque danseur·euse débutant dans les compétitions SCR4T commence dans la première division : la division initiée. 
-                    Iel y reste jusqu'à avoir obtenu suffisamment de succès compétitifs, pour lui ouvrir l'accès à la compétition dans la division suivante : la division intermédiaire. 
+                    Chaque danseur·euse débutant dans les compétitions SCR4T commence dans la première division : la division Initié. 
+                    Iel y reste jusqu'à avoir obtenu suffisamment de succès compétitifs, pour lui ouvrir l'accès à la compétition dans la division suivante : la division Intermédiaire. 
                     Iel accumule alors des points Intermédiaire, qui lui permettent d'obtenir l'accès à la division suivante : la division Avancé.
                 </p>
 
@@ -47,6 +52,9 @@ function HomePage() {
                     <li>Ajout d'un 6ème tier de compétition au-delà de 65 compétiteurs dans un rôle. <a href="/rules#phases">En savoir plus</a>.</li>
                     <li>Légère révision à la baisse de la grille des point obtenus selon le classement. <a href="/rules#points">En savoir plus</a>.</li>
                 </ul>
+                <div className="nb nb-warning">
+                    La réforme n'est pas rétroactive, les accès gagnés aux divisions avant le 9 juillet 2025 sont conservés.
+                </div><br/>
 
                 <h2>Nos engagements</h2>
                 <ul>
@@ -60,7 +68,7 @@ function HomePage() {
 
                 <h2>Contact</h2>
                 <p>Pour ne pas rater les nouvelles infos, suivez <a target="_blank" href="https://www.facebook.com/SCR4T.danse">notre page facebook</a> !</p>
-                <p>Vous êtes un organisateur souhaitant utiliser le système, un compétiteur curieux ? Contactez-nous à : <a href="mailto:scr4t.danse@gmail.com">scr4t.danse@gmail.com</a>.</p>
+                <p>Vous êtes un•e organisateur•ice souhaitant utiliser le système, un•e compétiteur•ice curieux•se ? Contactez-nous à : <a href="mailto:scr4t.danse@gmail.com">scr4t.danse@gmail.com</a>.</p>
             </div>
             <Footer />
         </>
