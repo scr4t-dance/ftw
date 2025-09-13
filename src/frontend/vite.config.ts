d3d76c6d13efa3cbb3427ed61778e35802d63263
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-09-13 13:32:57 +0200

    update deploy

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,8 +1,21 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log("API BASE URL:", env.API_BASE_URL);
+
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
