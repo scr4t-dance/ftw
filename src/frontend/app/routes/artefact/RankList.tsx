43fb140700d990c210cb94a6932baffa69137f65
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-09-13 10:51:59 +0200

    add promote button

--- src/frontend/app/routes/artefact/RankList.tsx
+++ src/frontend/app/routes/artefact/RankList.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { RoleItem, YanItem, type Artefact, type Bib, type DancerId, type DancerIdList, type HeatTargetJudgeArtefact, type PhaseId, type PhaseRanks, type Target, type TargetRank } from "@hookgen/model";
+import { RoleItem, YanItem, type Artefact, type Bib, type DancerId, type DancerIdList, type HeatTargetJudgeArtefact, type Phase, type PhaseId, type PhaseRanks, type Target, type TargetRank } from "@hookgen/model";
 import { Link, useParams } from "react-router";
 import { useGetApiPhaseId } from "@hookgen/phase/phase";
-import { useQueries } from "@tanstack/react-query";
+import { useQueries, useQueryClient } from "@tanstack/react-query";
 import { DancerCell } from '@routes/bib/BibList';
 import { getGetApiDancerIdQueryOptions } from '~/hookgen/dancer/dancer';
 import { useGetApiPhaseIdJudges } from '~/hookgen/judge/judge';
 import { useGetApiPhaseIdRanking } from '~/hookgen/ranking/ranking';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import NextPhaseForm from '../phase/NextPhaseForm';
 
-const rank_list_list = {
+const yan_rank_list_list = {
     ranks: [
         [
             {
@@ -29,6 +31,28 @@ const rank_list_list = {
         ]
     ]
 } as PhaseRanks;
+
+
+const rpss_rank_list_list: PhaseRanks = {
+    ranks: [
+        [
+            {
+                ranking_type: "rpss",
+                target: { target_type: "single", target: 1, role: [RoleItem.Follower] } as Target,
+                rank: 1,
+                artefact_list: [{ artefact_type: "ranking", artefact_data: 1 }, { artefact_type: "ranking", artefact_data: 1 }],
+                ranking_details: ["+1", "=2"]
+            } satisfies TargetRank,
+            {
+                ranking_type: "rpss",
+                target: { target_type: "single", target: 2, role: [RoleItem.Leader] } as Target,
+                rank: 1,
+                artefact_list: [{ artefact_type: "ranking", artefact_data: 1 }, { artefact_type: "ranking", artefact_data: 1 }],
+                ranking_details: ["+1", "=2"]
+            } satisfies TargetRank,
+        ]
+    ]
+} satisfies PhaseRanks;
 
 const iter_target_dancers = (t: Target) => t.target_type === "single"
     ? [t.target]
@@ -67,21 +91,14 @@ function RankRow({ target_rank, index }: { target_rank: TargetRank, index: numbe
             {(target_rank.artefact_list ?? []).map((rank) => (
                 <ArtefactCell artefact={rank} />
             ))}
+            {target_rank.ranking_type === "rpss" &&
+                <td>{target_rank.ranking_details}</td>
+            }
         </tr >
     );
 }
 
-function matchHeatBib(htja: HeatTargetJudgeArtefact, bib: Bib, heat_number: number | undefined) {
-    if (heat_number === undefined) {
-        return JSON.stringify(htja.heat_target_judge.target) === JSON.stringify(bib.target);
-    }
-
-    return (
-        JSON.stringify(htja.heat_target_judge.target) === JSON.stringify(bib.target)
-        && (htja.heat_target_judge.heat_number === heat_number));
-}
-
-export function RankListComponent({ phase_id, judges, head_judge, rank_list_list }: { phase_id: PhaseId, judges: DancerIdList, head_judge: DancerId | undefined, rank_list_list: PhaseRanks }) {
+function RankListTable({ phase_id, judges, head_judge, rank_list_list }: { phase_id: PhaseId, judges: DancerIdList, head_judge: DancerId | undefined, rank_list_list: PhaseRanks }) {
 
     const all_judges: DancerId[] = judges.dancers.concat(head_judge ? [head_judge] : []);
 
@@ -106,12 +123,18 @@ export function RankListComponent({ phase_id, judges, head_judge, rank_list_list
             }
         </div>);
 
+    const first_target_rank = rank_list_list.ranks[0][0];
+
     return (
         <table>
             <tbody>
                 <tr>
                     <th>Rank</th>
-                    <th>Score</th>
+                    {first_target_rank.ranking_type === "yan" &&
+                        <>
+                            <th>Score</th>
+                        </>
+                    }
                     <th>Target</th>
                     {judgeDataQueries.map((judgeQuery, index) => {
                         const judgeId = all_judges[index];
@@ -128,6 +151,11 @@ export function RankListComponent({ phase_id, judges, head_judge, rank_list_list
                             </th>
                         );
                     })}
+                    {first_target_rank.ranking_type === "rpss" &&
+                        <>
+                            <th>Ranking Details</th>
+                        </>
+                    }
                 </tr>
                 {rank_list_list && rank_list_list.ranks.flat().map((target_rank, index) => {
                     return (
@@ -142,55 +170,73 @@ export function RankListComponent({ phase_id, judges, head_judge, rank_list_list
     );
 }
 
+function RankListComponent({ id_phase, debug }: { id_phase: PhaseId, debug: String | undefined }) {
 
+    const { data: phaseData, isLoading } = useGetApiPhaseId(id_phase);
 
-export default function ArtefactList() {
+    const { data: _rank_list_list, isSuccess: isSuccessRank } = useGetApiPhaseIdRanking(id_phase);
 
-    let { id_phase } = useParams();
-    let id_phase_number = Number(id_phase) as PhaseId;
-    const { data: phaseData, isLoading } = useGetApiPhaseId(id_phase_number);
-
-    const { data: _rank_list_list, isSuccess: isSuccessRank } = useGetApiPhaseIdRanking(id_phase_number);
-
-    const { data: judgePanel, isSuccess: isSuccessJudges } = useGetApiPhaseIdJudges(id_phase_number);
+    const { data: judgePanel, isSuccess: isSuccessJudges } = useGetApiPhaseIdJudges(id_phase);
 
     if (isLoading) return <div>Chargement...</div>;
     if (!phaseData) return null;
+    // uncomment below when API works
     //if (!isSuccessRank) return <div>Chargement des classements...</div>;
     if (!isSuccessJudges) return <div>Chargement des juges...</div>;
 
+
+    const rank_list_list = (debug === undefined) ? _rank_list_list : (
+        debug === "yan" ? yan_rank_list_list : rpss_rank_list_list
+    );
+
     return (
         <>
-            <h1>Données simulées !!!!</h1>
             {judgePanel.panel_type === "single" && rank_list_list && rank_list_list.ranks &&
                 <>
                     <h1>Followers</h1>
-                    <RankListComponent
-                        phase_id={id_phase_number}
+                    <RankListTable
+                        phase_id={id_phase}
                         judges={judgePanel.followers}
                         head_judge={judgePanel.head}
-                        rank_list_list={{ranks:rank_list_list.ranks.map((u) => (u.filter((t) => t.target.target_type === "single" && t.target.role[0] === "Follower")))}}
+                        rank_list_list={{ ranks: rank_list_list.ranks.map((u) => (u.filter((t) => t.target.target_type === "single" && t.target.role[0] === "Follower"))) }}
                     />
                     <h1>Leaders</h1>
-                    <RankListComponent
-                        phase_id={id_phase_number}
+                    <RankListTable
+                        phase_id={id_phase}
                         judges={judgePanel.leaders}
                         head_judge={judgePanel.head}
-                        rank_list_list={{ranks:rank_list_list.ranks.map((u) => (u.filter((t) => t.target.target_type === "single" && t.target.role[0] === "Leader")))}}
+                        rank_list_list={{ ranks: rank_list_list.ranks.map((u) => (u.filter((t) => t.target.target_type === "single" && t.target.role[0] === "Leader"))) }}
                     />
                 </>
             }
             {judgePanel.panel_type === "couple" && rank_list_list && rank_list_list.ranks &&
                 <>
                     <h1>Couples</h1>
-                    <RankListComponent
-                        phase_id={id_phase_number}
+                    <RankListTable
+                        phase_id={id_phase}
                         judges={judgePanel.couples}
                         head_judge={judgePanel.head}
-                        rank_list_list={{ranks:rank_list_list.ranks.map((u) => (u.filter((t) => t.target.target_type === "couple")))}}
+                        rank_list_list={{ ranks: rank_list_list.ranks.map((u) => (u.filter((t) => t.target.target_type === "couple"))) }}
                     />
                 </>
             }
+        </>
+    );
+
+}
+
+export default function RankList() {
+
+    let { id_phase } = useParams();
+    let id_phase_number = Number(id_phase) as PhaseId;
+
+    return (
+        <>
+            <NextPhaseForm id_phase={id_phase_number} />
+            <h1>Données simulées YAN !!!!</h1>
+            <RankListComponent id_phase={id_phase_number} debug={"yan"} />
+            <h1>Données simulées RPSS !!!!</h1>
+            <RankListComponent id_phase={id_phase_number} debug={"rpss"} />
         </>
     );
 }
