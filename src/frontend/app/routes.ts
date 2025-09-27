a89146704695a331a1b6ab3357617f202025f7fd
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-09-27 15:06:28 +0200

    exclude admin pages from build

--- src/frontend/app/routes.ts
+++ src/frontend/app/routes.ts
@@ -1,16 +1,15 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [
-  index("routes/home.tsx"),
-  route("login", "routes/login.tsx"),
-  route("logout", "routes/logout.tsx"),
-  route("about", "routes/index/about.tsx"),
+const showAdmin = (import.meta.env.VITE_SHOW_ADMIN ?? process.env.VITE_SHOW_ADMIN) === "true";
+console.log("routes", showAdmin, import.meta.env.VITE_SHOW_ADMIN, process.env.VITE_SHOW_ADMIN);
+
+const admin_routes = !showAdmin ? [] : [
   route("admin", "routes/event/EventsHomeAdmin.tsx", [
     index("routes/event/EventListAdmin.tsx"),
     route("new", "routes/event/NewEventForm.tsx"),
     route(":id_event", "routes/event/EventDetailsHomeAdmin.tsx", [
       index("routes/event/EventDetailsAdmin.tsx"),
-      route("competitions", "routes/competition/EventCompetitionsHomeAdmin.tsx",[
+      route("competitions", "routes/competition/EventCompetitionsHomeAdmin.tsx", [
         index("routes/competition/CompetitionListAdmin.tsx"),
         route("new", "routes/competition/NewCompetitionForm.tsx"),
         route(":id_competition", "routes/competition/CompetitionHomeAdmin.tsx", [
@@ -33,7 +32,15 @@ export default [
         ]),
       ]),
     ]),
-  ]),
+  ])
+];
+
+export default [
+  index("routes/home.tsx"),
+  route("login", "routes/login.tsx"),
+  route("logout", "routes/logout.tsx"),
+  route("about", "routes/index/about.tsx"),
+  ...admin_routes,
   route("events", "routes/event/EventsHome.tsx", [
     index("routes/event/EventList.tsx"),
     //route(":id_event", "routes/event/EventDetailsNoForm.tsx"),
