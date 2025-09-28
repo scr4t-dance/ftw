e6194c5a00f043389c177c411483a79548fdde1c
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-09-28 14:10:37 +0200

    add dancer admin pages

--- src/frontend/app/routes/dancer/DancerPagePublic.tsx
+++ src/frontend/app/routes/dancer/DancerPagePublic.tsx
@@ -1,9 +1,11 @@
-import type { Route } from './+types/DancerPage';
+import type { Route } from './+types/DancerPagePublic';
 import React from 'react';
+import { useParams } from "react-router";
 
 import { type DancerId } from "@hookgen/model";
-import { getApiDancerId } from '@hookgen/dancer/dancer';
-import { DancerPageComponent } from './DancerComponents';
+import { getApiDancerId, useGetApiDancerId } from '@hookgen/dancer/dancer';
+import { DancerPageComponent } from '@routes/dancer/DancerComponents';
+
 
 export async function loader({ params }: Route.LoaderArgs) {
 
@@ -17,12 +19,14 @@ export async function loader({ params }: Route.LoaderArgs) {
     };
 }
 
-function DancerPage({loaderData}: Route.ComponentProps) {
+function DancerPagePublic({loaderData}:Route.ComponentProps) {
+
 
     const {id_dancer, dancer} = loaderData;
+
     return (
         <DancerPageComponent dancer={dancer} id_dancer={id_dancer} />
     );
 }
 
-export default DancerPage;
+export default DancerPagePublic;
