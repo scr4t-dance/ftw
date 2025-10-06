8f2b96cc2293a25ba7dc07924a23ce48d9e9eca0
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-10-07 01:24:00 +0200

    init search bar for bibs

--- src/frontend/app/routes/bib/NewBibForm.tsx
+++ src/frontend/app/routes/bib/NewBibForm.tsx
@@ -4,12 +4,16 @@ import React from 'react';
 import type { Route } from './+types/NewBibForm';
 import { NewBibFormComponent } from '@routes/bib/NewBibFormComponent';
 import { useGetApiCompIdBibs } from '@hookgen/bib/bib';
-import { bibsListLoader, combineClientLoader, combineServerLoader, competitionLoader, eventLoader, queryClient, type LoaderOutput, type WithEntityData } from '~/queryClient';
+import {
+    bibsListLoader, combineClientLoader, combineServerLoader, competitionLoader,
+    dancerListLoader, eventLoader, queryClient,
+} from '~/queryClient';
+import { useGetApiDancers } from '~/hookgen/dancer/dancer';
 
 
 
 
-const loader_array = [eventLoader, competitionLoader, bibsListLoader];
+const loader_array = [eventLoader, competitionLoader, bibsListLoader, dancerListLoader];
 
 
 export async function loader({ params }: Route.LoaderArgs) {
@@ -43,17 +47,22 @@ clientLoader.hydrate = true;
 function BibHomePublic({ loaderData }: Route.ComponentProps) {
 
     const { data: bibs_list } = useGetApiCompIdBibs(loaderData.id_competition, {
-            query: {
-                initialData: loaderData.bibs_list,
-            }
+        query: {
+            initialData: loaderData.bibs_list,
         }
-    );
+    });
+
+    const { data: dancer_list } = useGetApiDancers({
+        query: {
+            initialData: loaderData.dancer_list,
+        }
+    });
 
     return (
         <>
             <h1>Compétition {loaderData.competition_data.name}</h1>
             <h2>Ajouter une compétiteurice</h2>
-            <NewBibFormComponent id_competition={loaderData.id_competition} bibs_list={bibs_list} />
+            <NewBibFormComponent id_competition={loaderData.id_competition} bibs_list={bibs_list} dancer_list={dancer_list}/>
         </>
     );
 }
@@ -61,5 +70,5 @@ function BibHomePublic({ loaderData }: Route.ComponentProps) {
 export default BibHomePublic;
 
 export const handle = {
-  breadcrumb: () => "New"
+    breadcrumb: () => "New"
 };
