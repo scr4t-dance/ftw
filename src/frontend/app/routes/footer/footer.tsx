166a8936301e4668409cb979de66e724e00125b5
dowymn <down@Dowymn.localdomain>
2025-10-15 23:47:19 +0200

    login css + problem

--- src/frontend/app/routes/footer/footer.tsx
+++ src/frontend/app/routes/footer/footer.tsx
@@ -1,8 +1,28 @@
 import "./Footer.css";
+import { Link, useLocation, type LoaderFunctionArgs } from "react-router";
 
-export default function Footer() {
+export default function Footer({ userId }: { userId: string | null }) {
+
+    console.log("userId:",userId)
+
+    const location = useLocation();
+    let params = new URLSearchParams();
+    params.set("from", location.pathname);
+
+    const disable_admin = import.meta.env.VITE_DISABLE_ADMIN === "true";
+
     return (
         <footer>
+            {!disable_admin && userId &&
+                <div className="footer_buttons">
+                    <li className="footer_button"><Link to="/admin">Admin</Link></li>
+                    <li className="footer_button"><Link to={"/logout?" + params.toString()}>Déconnexion</Link></li>
+                </div>
+            }
+            {!disable_admin && !userId &&
+                <div className="footer_button"><Link to={"/login?" + params.toString()}>Se connecter</Link></div>
+            }
+
             <span>© 2025 SCR4T</span>
         </footer>
     );
