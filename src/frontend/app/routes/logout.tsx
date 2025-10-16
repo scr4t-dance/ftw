69ec29ac3f874ebd80306a6b2076fcecccaf67d5
dowymn <down@Dowymn.localdomain>
2025-10-16 13:38:26 +0200

    deconnexion form css

--- src/frontend/app/routes/logout.tsx
+++ src/frontend/app/routes/logout.tsx
@@ -1,36 +1,39 @@
 import { Form, Link, redirect, useLocation } from "react-router";
 import {
-  getSession,
-  destroySession,
+	getSession,
+	destroySession,
 } from "~/sessions.server";
 import type { Route } from "./+types/logout";
 
 export async function action({
-  request,
+	request,
 }: Route.ActionArgs) {
-  const session = await getSession(
-    request.headers.get("Cookie"),
-  );
-  return redirect("/login", {
-    headers: {
-      "Set-Cookie": await destroySession(session),
-    },
-  });
+	const session = await getSession(
+		request.headers.get("Cookie"),
+	);
+	return redirect("/login", {
+		headers: {
+			"Set-Cookie": await destroySession(session),
+		},
+	});
 }
 
 export default function LogoutRoute() {
 
-  let location = useLocation();
-  let params = new URLSearchParams(location.search);
-  let from = params.get("from") || "/";
+	let location = useLocation();
+	let params = new URLSearchParams(location.search);
+	let from = params.get("from") || "/";
 
-  return (
-    <>
-      <p>Are you sure you want to log out?</p>
-      <Form method="post">
-        <button>Logout</button>
-      </Form>
-      <Link to={from}>Never mind</Link>
-    </>
-  );
+	return (
+		<div className="logout_container">
+			<h3>Êtes-vous sûr•e de vouloir vous déconnecter ?</h3>
+
+			<div className="row">
+				<Form method="post" className="logout_form">
+					<button className="btn">Déconnexion</button>
+				</Form>
+				<Link to={from} className="link_button">Annuler</Link>
+			</div>
+		</div>
+	);
 }
