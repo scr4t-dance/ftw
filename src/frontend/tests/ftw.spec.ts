4ad5913cb9ea2ad1e73377394a0863887ca970f1
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-10-16 22:11:55 +0200

    randomize event and competition names

--- src/frontend/tests/ftw.spec.ts
+++ src/frontend/tests/ftw.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -8,18 +9,22 @@ test('has title', async ({ page }) => {
 });
 
 test('create event', async ({ page }) => {
+
+  const randomEventName = faker.string.alpha(10);
+  const randomCompetitionName = faker.string.alpha(10);
+
   await page.goto('http://localhost:3000/');
   await page.getByRole('link', { name: 'Événements' }).click();
   await page.getByRole('link', { name: 'Créer un nouvel événement' }).click();
   await page.locator('input[name="name"]').click();
-  await page.locator('input[name="name"]').fill('Test playwright');
+  await page.locator('input[name="name"]').fill(randomEventName);
   await page.locator('div').filter({ hasText: /^Date de début$/ }).getByRole('textbox').fill('2025-09-17');
   await page.locator('div').filter({ hasText: /^Date de fin$/ }).getByRole('textbox').fill('2025-09-26');
   await page.getByRole('button', { name: 'Valider l\'événement' }).click();
   await page.getByRole('link', { name: 'Accéder à l\'événement' }).click();
   await page.getByRole('link', { name: 'Créer une competition' }).click();
   await page.getByRole('textbox').click();
-  await page.getByRole('textbox').fill('test playwright comp');
+  await page.getByRole('textbox').fill(randomCompetitionName);
   await page.locator('select[name="kind.0"]').selectOption('Strictly');
   await page.locator('select[name="category.0"]').selectOption('Advanced');
   await page.getByRole('button', { name: 'Créer la compétition' }).click();
