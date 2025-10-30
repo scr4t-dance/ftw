5eec54e2e70ceaed1ae48213c3280f31aa3e5326
Xavier DUQUESNE <duquesne.xavier.13@gmail.com>
2025-10-30 23:00:02 +0100

    simplify tests to handle safari browser

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -44,11 +44,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    // Cookies not working on Safari
-    //{
-    //  name: 'webkit',
-    //  use: { ...devices['Desktop Safari'] },
-    //},
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
